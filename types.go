a5bec13fe530dd6f331c0673fbed752d6c545066
Xevion <xevion@xevion.dev>
2024-01-18 03:11:23 -0600

    Remove unused MeetingTime pretty structs

--- types.go
+++ types.go
@@ -169,30 +169,3 @@ type SearchResult struct {
 		MeetingsFaculty                []MeetingTimeResponse `json:"meetingsFaculty"`
 	} `json:"data"`
 }
-
-type MeetingTimeFaculty struct {
-	bannerId    int
-	category    string
-	displayName string
-	email       string
-	primary     bool
-}
-
-type PrettyMeetingTimeResponse struct {
-	faculty                []MeetingTimeFaculty
-	weekdays               map[time.Weekday]bool
-	campus                 string
-	campusDescription      string
-	creditHours            int
-	building               string
-	buildingDescription    string
-	room                   string
-	timeStart              NaiveTime
-	timeEnd                NaiveTime
-	dateStart              time.Time
-	dateEnd                time.Time
-	hoursPerWeek           float32
-	meetingScheduleType    string
-	meetingType            string
-	meetingTypeDescription string
-}
